1792419b60a126bac6c7e6d7eadf5b15c309bd40
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-09-01 00:06:39 -0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", event => {
   let ja = document.getElementById("janj");
   audio.volume = 1.0;
   ja.addEventListener("click", () => {
-    ((Math.floor(Math.random() * 4)) == 3) ? audio.setAttribute("src", "https://cdn.discordapp.com/attachments/810799100940255260/879617801185280020/janja_on_a_ship_HD_720p_1.mp3") : audio.setAttribute("src", "https://cdn.discordapp.com/attachments/810799100940255260/879598735011770439/yt1s.com_-_Fly_me_to_the_moonEvangelion.mp3");
+    ((Math.floor(Math.random() * 4)) == 3) ? audio.setAttribute("src", "ship.mp3") : audio.setAttribute("src", "moon.mp3");
     audio.paused ? audio.play() : audio.pause();
   });
   audio.play();
