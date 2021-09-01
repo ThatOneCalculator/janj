f1b5e4c8e2add56017041d7d2f8da93bb978faff
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-09-01 00:11:56 -0700

    im goin insane for this lil dude

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", event => {
-  let audio = document.querySelector("audio");
-  let ja = document.getElementById("janj");
+  const audio = document.querySelector("audio");
+  const ja = document.getElementById("janj");
   audio.volume = 1.0;
   ja.addEventListener("click", () => {
     ((Math.floor(Math.random() * 4)) == 3) ? audio.setAttribute("src", "ship.mp3") : audio.setAttribute("src", "moon.mp3");
